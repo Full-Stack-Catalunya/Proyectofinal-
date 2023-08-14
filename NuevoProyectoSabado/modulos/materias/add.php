9ef711919e053be458d42792d1081a71129e2aa6
Jhomathan01 <111469561+Jhomathan01@users.noreply.github.com>
2023-08-14 16:24:12 -0500

    Update add.php

--- NuevoProyectoSabado/modulos/materias/add.php
+++ NuevoProyectoSabado/modulos/materias/add.php
@@ -6,10 +6,10 @@
     if(isset($_POST['nombre']))      $nombre = $_POST['nombre'];
     if(isset($_POST['jornada']))      $horario = $_POST['jornada']; 
     if(isset($_POST['docente']))      $docente = $_POST['docente']; 
-    if(isset($_POST['descripcion']))  $descripcion = $_POST['descripcion'];
+    if(isset($_POST['area']))  $descripcion = $_POST['area'];
 
     $conexion = new Database;  
-    $result = $conexion->CrearMateria($nombre,$jornada,$docente,$descripcion);
+    $result = $conexion->CrearMateria($nombre,$jornada,$docente,$area);
 
     header("Location: ".ROOT."modulos/materias/materias.php?mensaje=".$result);
 
