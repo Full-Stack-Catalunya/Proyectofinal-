eae3d4ef2fc5f01e13bc9e34810620a540c707c8
Jhomathan01 <111469561+Jhomathan01@users.noreply.github.com>
2023-08-14 16:34:33 -0500

    Update add.php
    cambios termino

--- NuevoProyectoSabado/modulos/materias/add.php
+++ NuevoProyectoSabado/modulos/materias/add.php
@@ -4,9 +4,9 @@
     include_once("../../config/Config.php");    
 
     if(isset($_POST['nombre']))      $nombre = $_POST['nombre']; 
-    if(isset($_POST['jornada']))      $nombre = $_POST['jornada']; 
-    if(isset($_POST['docente']))      $nombre = $_POST['docente']; 
-    if(isset($_POST['area']))      $nombre = $_POST['area']; 
+    if(isset($_POST['jornada']))      $jornada = $_POST['jornada']; 
+    if(isset($_POST['docente']))      $docente = $_POST['docente']; 
+    if(isset($_POST['area']))      $area = $_POST['area']; 
 
     $conexion = new Database;  
     $result = $conexion->CrearMateria($nombre,$jornada,$docente,$area);
