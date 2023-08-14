c7169e836d3e154e62fd85ceec01c8e4084a1a9e
Jhomathan01 <111469561+Jhomathan01@users.noreply.github.com>
2023-08-14 15:19:23 -0500

    Update add.php

--- NuevoProyectoSabado/modulos/materias/add.php
+++ NuevoProyectoSabado/modulos/materias/add.php
@@ -3,10 +3,13 @@
     include_once("../../config/DBConect.php");
     include_once("../../config/Config.php");    
 
-    if(isset($_POST['nombre']))      $nombre = $_POST['nombre']; 
+    if(isset($_POST['nombre']))      $nombre = $_POST['nombre'];
+    if(isset($_POST['jornada']))      $horario = $_POST['jornada']; 
+    if(isset($_POST['docente']))      $docente = $_POST['docente']; 
+    if(isset($_POST['descripcion']))  $descripcion = $_POST['descripcion'];
 
     $conexion = new Database;  
-    $result = $conexion->CrearMateria($nombre);
+    $result = $conexion->CrearMateria($nombre,$jornada,$docente,$descripcion);
 
     header("Location: ".ROOT."modulos/materias/materias.php?mensaje=".$result);
 
